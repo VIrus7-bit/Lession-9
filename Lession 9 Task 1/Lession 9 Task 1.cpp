e0eb6a8fa5cb6bd7804713bec90bc514904b73a5
VIrus7-bit <italyfotura7@gmail.com>
2024-09-10 10:24:54 -0700

    Lession 9

---
+++ Lession 9 Task 1/Lession 9 Task 1.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Fraction
+{
+	friend bool operator == (const Fraction& f1, const Fraction f2);
+	friend bool operator != (const Fraction& f1, const Fraction f2);
+	friend bool operator <  (const Fraction& f1, const Fraction f2);
+	friend bool operator >  (const Fraction& f1, const Fraction f2);
+	friend bool operator <= (const Fraction& f1, const Fraction f2);
+	friend bool operator >= (const Fraction& f1, const Fraction f2);
+
+private:
+	int numerator_{};
+	int denominator_{};
+
+public: Fraction(int numerator, int denominator) : numerator_{ numerator }, denominator_{ denominator } {}
+};
+
+bool operator==(const Fraction& f1, const Fraction f2)
+{
+	return (f1.numerator_ ==f2.numerator_ && f1.denominator_ == f2.denominator_);
+}
+
+bool operator!=(const Fraction& f1, const Fraction f2)
+{
+	return (f1.numerator_ != f2.numerator_ && f1.denominator_ != f2.denominator_);
+}
+
+bool operator<(const Fraction& f1, const Fraction f2)
+{
+	return (f1.numerator_ < f2.numerator_ && f1.denominator_ < f2.denominator_);
+}
+
+bool operator>(const Fraction& f1, const Fraction f2)
+{
+	return (f1.numerator_ > f2.numerator_ && f1.denominator_ > f2.denominator_);
+}
+
+bool operator<=(const Fraction& f1, const Fraction f2)
+{
+	return (f1.numerator_ <= f2.numerator_ && f1.denominator_ <= f2.denominator_);
+}
+
+bool operator>=(const Fraction& f1, const Fraction f2)
+{
+	return (f1.numerator_ >= f2.numerator_ && f1.denominator_ >= f2.denominator_);
+}
+
+
+
+int main() {
+
+	setlocale(LC_ALL, "Rus");
+	system("chcp 1251");
+	system("cls");
+
+	Fraction f1(4, 3);
+	Fraction f2(6, 11);
+
+	cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	cout << "f1" << ((f1  < f2) ? " <  " : " not <  ") << "f2" << '\n';
+	cout << "f1" << ((f1  > f2) ? " >  " : " not >  ") << "f2" << '\n';
+	cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+	
+	system("pause");
+	return 0;
+}
+
+
---
+++ Lession 9 Task 2/Lession 9 Task 2.cpp
@@ -0,0 +1,107 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Fraction
+{
+	friend Fraction operator +  (const Fraction& f1, const Fraction f2);
+	friend Fraction operator -  (const Fraction& f1, const Fraction f2);
+	friend Fraction operator *  (const Fraction& f1, const Fraction f2);
+	friend Fraction operator /  (const Fraction& f1, const Fraction f2);
+	friend bool operator == (const Fraction& f1, const Fraction f2);
+	friend bool operator != (const Fraction& f1, const Fraction f2);
+	friend bool operator <  (const Fraction& f1, const Fraction f2);
+	friend bool operator >  (const Fraction& f1, const Fraction f2);
+	friend bool operator <= (const Fraction& f1, const Fraction f2);
+	friend bool operator >= (const Fraction& f1, const Fraction f2);
+
+private:
+	int numerator_{};
+	int denominator_{};
+
+public: Fraction(int numerator, int denominator) : numerator_{ numerator }, denominator_{ denominator } {}
+};
+
+Fraction operator+(const Fraction& f1, const Fraction f2)
+{
+	return Fraction{ f1.numerator_ + f2.numerator_, f1.denominator_ + f2.denominator_ };
+}
+
+Fraction operator-(const Fraction& f1, const Fraction f2)
+{
+	return Fraction{ f1.numerator_ - f2.numerator_, f1.denominator_ - f2.denominator_ };
+}
+
+Fraction operator*(const Fraction& f1, const Fraction f2)
+{
+	return Fraction{ f1.numerator_ * f2.numerator_, f1.denominator_ * f2.denominator_ };
+}
+
+Fraction operator/(const Fraction& f1, const Fraction f2)
+{
+	return Fraction{ f1.numerator_ / f2.numerator_, f1.denominator_ / f2.denominator_ };
+}
+
+bool operator==(const Fraction& f1, const Fraction f2)
+{
+	return (f1.numerator_ == f2.numerator_ && f1.denominator_ == f2.denominator_);
+}
+
+bool operator!=(const Fraction& f1, const Fraction f2)
+{
+	return (f1.numerator_ != f2.numerator_ && f1.denominator_ != f2.denominator_);
+}
+
+bool operator<(const Fraction& f1, const Fraction f2)
+{
+	return (f1.numerator_ < f2.numerator_ && f1.denominator_ < f2.denominator_);
+}
+
+bool operator>(const Fraction& f1, const Fraction f2)
+{
+	return (f1.numerator_ > f2.numerator_ && f1.denominator_ > f2.denominator_);
+}
+
+bool operator<=(const Fraction& f1, const Fraction f2)
+{
+	return (f1.numerator_ <= f2.numerator_ && f1.denominator_ <= f2.denominator_);
+}
+
+bool operator>=(const Fraction& f1, const Fraction f2)
+{
+	return (f1.numerator_ >= f2.numerator_ && f1.denominator_ >= f2.denominator_);
+}
+
+
+
+int main() {
+
+	setlocale(LC_ALL, "Rus");
+	system("chcp 1251");
+	system("cls");
+
+	double a, b, c, d;
+	cout << "Введите числитель дроби 1:"; cin >> a;
+	cout << "Введите знаменатель дроби 1:"; cin >> b;
+	cout << "Введите числитель дроби 2:"; cin >> c;
+	cout << "Введите знаменатель дроби 2:"; cin >> d;
+
+	Fraction f1(a, b);
+	Fraction f2(c, d);
+
+	cout << a << "/" << b << " + " << c << "/" << d << " = " << ((a / b + c / d)) << endl;
+	cout << a << "/" << b << " - " << c << "/" << d << " = " << ((a / b - c / d)) << endl;
+	cout << a << "/" << b << " * " << c << "/" << d << " = " << ((a / b * c / d)) << endl;
+	cout << a << "/" << b << " / " << c << "/" << d << " = " << ((a / b / c / d)) << endl;
+	cout << "++" << a << "/" << b << " * " << c << "/" << d << " = " << ((a / b * c / d)) << endl;
+	cout << "Значения дроби " << endl;
+	//Я обсалютно уверен что я делаю всё не так.
+
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
+
+
